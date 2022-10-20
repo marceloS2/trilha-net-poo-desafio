b98a9598fdf36e690dc2b70acabc1cb5010a84af
marceloS2 <celu.mar@hotmail.com>
2022-10-20 12:50:26 -0300

    Fanalizado

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        private string Modelo {get; set;}
+        private string IMEI {get;set;}
+        private int  Memoria {get;set;}
+        
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+          
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -20,7 +27,12 @@ namespace DesafioPOO.Models
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
+         
         public abstract void InstalarAplicativo(string nomeApp);
+        
+        public void Exibir()
+        {
+            Console.WriteLine($"Seu Smarphone é um : {Modelo}: Seu Numero :{Numero}: IMEI :{IMEI}: {Memoria}GB.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Nokia android = new Nokia("919425-55","NokiA15","AAAAAAAAA", 32);
+Iphone ios = new Iphone("919233-22", "Iphone30","BBBBBBB", 126);
+ios.Ligar();
+ios.ReceberLigacao();
+ios.InstalarAplicativo("Apple Store: Facebook");
+ios.Exibir();
+Console.WriteLine("----------------------------------------------------------------------------");
+android.Ligar();
+android.ReceberLigacao();
+android.InstalarAplicativo("Play Strore: Dio");
+android.Exibir();
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
